b6f0c0b3103d4f5d10e7339cbc4d86489079fb31
김대익 <33992354+dik654@users.noreply.github.com>
2023-07-31 06:01:28 +0000

    test

--- ch03/pkgquery/pkgquery_test.go
+++ ch03/pkgquery/pkgquery_test.go
@@ -12,20 +12,26 @@ func startTestPackageServer() *httptest.Server {
 {"name": "package1", "version": "1.1"}, 
 {"name": "package2", "version": "1.0"} 
 ]` 
+	// 서버에 요청이 들어오면 항상 아래와 같은 함수 실행
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { 
-			w.Header().Set("Content-Type", "application/json") 
-			fmt.Fprint(w, pkgData) 
+		// 헤더에서 컨텐츠 타입을 json으로 설정
+		w.Header().Set("Content-Type", "application/json") 
+		// 직렬화된 pkgData 리턴
+		fmt.Fprint(w, pkgData) 
 	})) 
 
 	return ts 
 }
 func TestFetchPackageData(t *testing.T) {
 	ts := startTestPackageServer()
+	// 테스트가 끝나면 테스트서버 닫기
 	defer ts.Close()
+	// 테스트 서버 url에서 데이터 가져오기
 	packages, err := fetchPackageData(ts.URL)
 	if err != nil {
 		t.Fatal(err)
 	}
+	// 2개의 json 데이터가 들어왔는지 체크
 	if len(packages) != 2 {
 		t.Fatalf("Expected 2 packages, Got back: %d", len(packages))
 	}
